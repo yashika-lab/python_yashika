7e45af49262332c6aca70fc60c0a8f00948c5630
yashika chugh <chughyashika0@gmail.com>
2024-07-16 00:45:56 +0530

    calulator add

---
+++ minicalculator.py
@@ -0,0 +1,17 @@
+a=int(input("enter your first number"))
+b=int(input("enter your second number"))
+c=input("enter the operation to perform:-")
+if c == "+":
+    d=a+b
+    print(d)
+if c == "-":
+    d=a-b
+    print(d)
+
+if c== "/":
+    d=(a/b)
+    print(d)
+
+if c== "*" :
+    d=(a*b)
+    print(d)
